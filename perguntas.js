410c189ec38e0f5fd6cafa4a0832a065a8203d58
indoma987 <161834677+indoma987@users.noreply.github.com>
2024-09-17 11:21:47 -0300

    tgyht

---
+++ perguntas.js
@@ -0,0 +1,122 @@
+criaCartao(
+    'Química',
+
+    'Como se calcula a concentração comum?',
+    'R: c=ms/vsol')
+
+criaCartao(
+    'Química',
+
+    'Como é separado as soluções?',
+    'R: solucao= soluto + solvente')
+
+criaCartao(
+    'Química',
+
+    'O que é entalpia e como se calcula?',
+    'R: calor liberado ou absorvido pelo sistema.  ∆H=hp-hr')
+
+criaCartao(
+    'Química',
+
+    'O que a termoquímica estuda?',
+    'R: estuda a temperatura das reações (calor e energia)')
+
+criaCartao(
+    'Química',
+
+    'Qual ligação compartilha elétrons?',
+    'R: ligação covalente')
+
+criaCartao(
+    'Física',
+
+    'O que é a Lei da Gravitação Universal?',
+    'R= Lei da Gravitação Universal, formulada por Isaac Newton, afirma que todos os corpos no universo se atraem mutuamente com uma força que é diretamente proporcional ao produto de suas massas e inversamente proporcional ao quadrado da distância entre seus centros')
+
+criaCartao(
+    'Física',
+
+    'O que é energia cinética?',
+    'R= Energia cinética é a energia que um corpo possui devido ao seu movimento')
+
+criaCartao(
+    'Física',
+
+    'O que é a Lei de Ohm?',
+    'R= A Lei de Ohm estabelece que a corrente elétrica (I) que passa por um condutor entre dois pontos é diretamente proporcional à tensão (V) entre esses pontos e inversamente proporcional à resistência (R) do condutor')
+
+criaCartao(
+    'Física',
+
+    'O que são ondas sonoras?',
+    'R= Ondas sonoras são ondas mecânicas longitudinais que se propagam em meios materiais, como ar, água ou sólidos. Elas são geradas por vibrações e podem ser percebidas pelo ouvido humano como som')
+
+criaCartao(
+    'Física',
+
+    'O que é a primeira lei de Newton (Lei da Inércia)?',
+    'R= A primeira lei de Newton, também conhecida como Lei da Inércia, afirma que um corpo em repouso permanecerá em repouso e um corpo em movimento continuará em movimento retilíneo uniforme a menos que uma força externa atue sobre ele')
+
+    criaCartao(
+        'Matemática',
+        
+        'O que são ângulos complementares?',
+        'R= Ângulos complementares são dois ângulos cuja soma é igual a 90 graus')
+        
+        criaCartao(
+        'Matemática',
+        
+        'O que é uma equação do segundo grau?',
+        'R= Uma equação do segundo grau é uma equação da forma ax² + bx + c = 0, onde a, b e c são constantes e a ≠ 0. As soluções podem ser encontradas usando a fórmula de Bhaskara')
+        
+        criaCartao(
+        'Matemática',
+        
+        'Como se calcula a média aritmética?',
+        'R= A média aritmética é calculada somando todos os valores e dividindo pelo número total de valores')
+        
+        criaCartao(
+        'Matemática',
+        
+        'O que são números inteiros?',
+        'R= Números inteiros são todos os números sem frações ou decimais, incluindo números negativos, zero e números positivos')
+        
+        criaCartao(
+        'Matemática',
+        
+        'O que é um ângulo reto?',
+        'R=Um ângulo reto é um ângulo que mede exatamente 90 graus. Ele é formado quando duas linhas se encontram de maneira a criar um "L" perfeito e é um dos tipos básicos de ângulos em geometria')
+
+        criaCartao(
+            'Biologia',
+            
+            'O que é a cadeia alimentar?',
+            'R= É uma sequência de organismos onde cada um serve de alimento para o próximo')
+            
+            criaCartao(
+            'Biologia',
+            
+            'O que é a teoria da evolução?',
+            'R= A teoria da evolução, proposta por Charles Darwin, sugere que as espécies se desenvolvem ao longo do tempo por meio de um processo chamado seleção natural, onde os organismos mais bem adaptados ao seu ambiente têm mais chances de sobreviver e reproduzir')
+            
+            criaCartao(
+            'Biologia',
+            
+            'Quais são os principais tipos de tecido no corpo humano?',
+            'R= Os principais tipos de tecido são: Tecido epitelial, tecido conjuntivo, tecido muscular, tecido nervoso')
+            
+            criaCartao(
+            'Biologia',
+            
+            'O que são organismos autotróficos e heterotróficos?',
+            'R=Organismos autotróficos produzem seu próprio alimento (como as plantas, que realizam fotossíntese), enquanto organismos heterotróficos obtêm alimento consumindo outros organismos (como os animais)')
+            
+            criaCartao(
+            'Biologia',
+            
+            'O que é o DNA e qual a sua função?',
+            'R= O DNA (ácido desoxirribonucleico) é uma molécula que contém as informações genéticas necessárias para o desenvolvimento, funcionamento e reprodução dos organismos. Ele é composto por duas cadeias que se entrelaçam em uma estrutura chamada dupla hélice')
+
+            
+            
